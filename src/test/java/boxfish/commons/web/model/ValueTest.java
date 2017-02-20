82ed6dd5a5ea803cf60269472f1e33a50c62932b
Hudson Mendes <hudsonmendes@gmail.com>
2017-02-20 16:53:39 +0000

    Possibility of conversion of values to Enum

--- src/test/java/boxfish/commons/web/model/ValueTest.java
+++ src/test/java/boxfish/commons/web/model/ValueTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.Thread.State;
 import java.math.BigDecimal;
 import java.time.Instant;
 
@@ -13,83 +14,89 @@ public class ValueTest {
 
     @Test
     public void asString() throws Exception {
-        String expected = "text";
+        final String expected = "text";
         assertEquals(expected, new Value("text").asString());
     }
 
     @Test
     public void asLong() throws Exception {
-        long expected = 12l;
+        final long expected = 12l;
         assertEquals(expected, new Value(12l).asLong().longValue());
     }
 
     @Test
     public void asInteger() throws Exception {
-        int expected = 15;
+        final int expected = 15;
         assertEquals(expected, new Value(expected).asInteger().intValue());
     }
 
     @Test
     public void asShort() throws Exception {
-        short expected = (short) 77;
+        final short expected = (short) 77;
         assertEquals(expected, new Value(expected).asShort().shortValue());
     }
 
     @Test
     public void asByte() throws Exception {
-        byte expected = (byte) 245;
+        final byte expected = (byte) 245;
         assertEquals(expected, new Value(expected).asByte().byteValue());
     }
 
     @Test
     public void asBoolean() throws Exception {
-        boolean expected = true;
+        final boolean expected = true;
         assertEquals(expected, new Value(expected).asBoolean().booleanValue());
     }
 
     @Test
     public void asBigDecimal() throws Exception {
-        BigDecimal expected = new BigDecimal("123.34");
+        final BigDecimal expected = new BigDecimal("123.34");
         assertEquals(expected, new Value(expected).asBigDecimal());
     }
 
     @Test
     public void asFloat() throws Exception {
-        Float expected = new Float("12.3");
+        final Float expected = new Float("12.3");
         assertEquals(expected, new Value(expected).asFloat());
     }
 
     @Test
     public void asDouble() throws Exception {
-        Double expected = new Double("12.3");
+        final Double expected = new Double("12.3");
         assertEquals(expected, new Value(expected).asDouble());
     }
 
     @Test
     public void asInstant() throws Exception {
-        Instant expected = Instant.now();
+        final Instant expected = Instant.now();
         assertEquals(expected, new Value(expected).asInstant());
     }
 
     @Test
     public void asModel() throws Exception {
-        Model expected = new Model().permit("field1").value("field1", "test");
+        final Model expected = new Model().permit("field1").value("field1", "test");
         assertEquals(
             expected.get("field1").asString(),
             new Value(expected).asModel().get("field1").asString());
     }
 
     @Test
+    public void asEnum() throws Exception {
+        final State expected = State.BLOCKED;
+        assertEquals(expected, new Value("BLOCKED").asEnum(State.class));
+    }
+
+    @Test
     public void asOriginal() {
-        BigDecimal expected = new BigDecimal("12.3");
+        final BigDecimal expected = new BigDecimal("12.3");
         assertEquals(expected, new Value(expected).asOriginal());
     }
 
     @Test
     public void getValueClass() throws Exception {
-        BigDecimal originalDecimal = new BigDecimal("12.3");
+        final BigDecimal originalDecimal = new BigDecimal("12.3");
         assertEquals(BigDecimal.class, new Value(originalDecimal).getValueClass());
-        String originalString = "4525j4";
+        final String originalString = "4525j4";
         assertEquals(String.class, new Value(originalString).getValueClass());
     }
 
