c8dc4f753611353910723ab4dd9db4afc0f33dda
Hudson Mendes <hudsonmendes@gmail.com>
2017-02-20 16:53:39 +0000

    Possibility of conversion of values to Enum

--- src/main/java/boxfish/commons/web/model/Value.java
+++ src/main/java/boxfish/commons/web/model/Value.java
@@ -8,6 +8,7 @@ import boxfish.commons.web.model.converters.ValueToBigDecimal;
 import boxfish.commons.web.model.converters.ValueToBoolean;
 import boxfish.commons.web.model.converters.ValueToByte;
 import boxfish.commons.web.model.converters.ValueToDouble;
+import boxfish.commons.web.model.converters.ValueToEnum;
 import boxfish.commons.web.model.converters.ValueToFloat;
 import boxfish.commons.web.model.converters.ValueToInstant;
 import boxfish.commons.web.model.converters.ValueToInteger;
@@ -20,7 +21,7 @@ import boxfish.commons.web.model.converters.ValueToString;
 /**
  * Wrapps the original value allowing easy type
  * conversions and obvious parsings.
- * 
+ *
  * @author Hudson Mendes
  *
  */
@@ -33,7 +34,7 @@ public class Value {
 
     /**
      * Presents the value as String.
-     * 
+     *
      * @return a string representing the value
      * @throws Exception raised if the type conversion fails.
      */
@@ -44,7 +45,7 @@ public class Value {
     /**
      * Presents the value as long. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return a long representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -55,7 +56,7 @@ public class Value {
     /**
      * Presents the value as Integer. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return an integer representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -66,7 +67,7 @@ public class Value {
     /**
      * Presents the value as Short. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return a short representing the value
      * @throws Exception raised if the type conversion fails.
      */
@@ -77,7 +78,7 @@ public class Value {
     /**
      * Presents the value as Byte. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return a byte representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -87,7 +88,7 @@ public class Value {
 
     /**
      * Presents the value as Boolean.
-     * 
+     *
      * @return a boolean representing a value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -98,7 +99,7 @@ public class Value {
     /**
      * Presents the value as BigDecimal. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return a BigDecimal representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -109,7 +110,7 @@ public class Value {
     /**
      * Presets the value as Float. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return a Float representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -120,7 +121,7 @@ public class Value {
     /**
      * Presets the value as Double. Works on most
      * of Numeric types and numeric Strings.
-     * 
+     *
      * @return a Double representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -130,7 +131,7 @@ public class Value {
 
     /**
      * Presents the value as Instant.
-     * 
+     *
      * @return an Instant representing the value.
      * @throws Exception raised if the type conversion fails.
      */
@@ -142,7 +143,7 @@ public class Value {
      * Presents the value as a Model. Used for nested
      * objects that may come in a JSON like or similar
      * structures.
-     * 
+     *
      * @return a Model representing the value when it's a complex object.
      * @throws Exception raised if the type conversion fails.
      */
@@ -151,8 +152,18 @@ public class Value {
     }
 
     /**
+     * Presents the value as an item of an Enum.
+     *
+     * @param enumType the type of the Enum.
+     * @return the value of the Enum that was parsed from the original value.
+     */
+    public <TEnum extends Enum<TEnum>> TEnum asEnum(final Class<TEnum> enumType) {
+        return new ValueToEnum<>(value, enumType).parse();
+    }
+
+    /**
      * Presents the value as a List of a particular type.
-     * 
+     *
      * @param clazz the type of the entry in the list.
      * @return a List<Value> representing the value.
      * @throws Exception raised if the type conversion fails.
@@ -163,7 +174,7 @@ public class Value {
 
     /**
      * Presents the original value without type conversion.
-     * 
+     *
      * @return the original value.
      */
     public Object asOriginal() {
@@ -173,7 +184,7 @@ public class Value {
     /**
      * Presents the Class<?> of the value, or
      * Object.class when the value is null.
-     * 
+     *
      * @return the class of the value.
      */
     public Class<?> getValueClass() {
@@ -184,7 +195,7 @@ public class Value {
 
     /**
      * Exposes if the object is null without an equality check.
-     * 
+     *
      * @return true if null, false otherwise.
      */
     public boolean isNull() {
---
+++ src/test/java/boxfish/commons/web/model/converters/ValueToEnumTest.java
@@ -0,0 +1,87 @@
+package boxfish.commons.web.model.converters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.lang.Thread.State;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import org.junit.Test;
+
+import boxfish.commons.web.model.Model;
+
+public class ValueToEnumTest {
+
+    @Test
+    public void parse_from_string() throws Exception {
+        final String expected = "BLOCKED";
+        assertEquals(State.BLOCKED, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_boolean() throws Exception {
+        final Boolean expected = true;
+        assertNull(new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_byte() throws Exception {
+        final byte expected = (byte) 1;
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_decimal() throws Exception {
+        final BigDecimal expected = new BigDecimal("1");
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_float() throws Exception {
+        final Float expected = Float.valueOf("1");
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_double() throws Exception {
+        final Double expected = Double.valueOf("1");
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_instant() throws Exception {
+        final Instant expected = Instant.now();
+        assertNull(new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_short() throws Exception {
+        final Short expected = 1;
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_integer() throws Exception {
+        final Integer expected = 1;
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_long() throws Exception {
+        final Long expected = 1l;
+        assertEquals(State.RUNNABLE, new ValueToEnum<>(expected, State.class).parse());
+    }
+
+    @Test
+    public void parse_from_model() throws Exception {
+        final Model expected = new Model()
+            .permit("field1", "field2", "field3", "field4")
+            .value("field1", "1341234asdasds")
+            .value("field2", Long.valueOf(123481))
+            .value("field3", new Model().permit("subField1").value("subField1", 1))
+            .value("field4", true);
+        assertNull(new ValueToEnum<>(expected, State.class).parse());
+    }
+
+}
